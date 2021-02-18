a4284ddc3de49f149cd3a406d1c1625c83ea3ca6
Clayton Cook <claytonleonardcook@gmail.com>
2021-02-18 14:04:27 -0500

    Refactored player script and added shooting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody body;
+    private Transform gun;
     Quaternion rotationAngle = new Quaternion();
     public float speed = 1f;
     float rotationSpeed = 2.5f;
@@ -12,6 +13,7 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         this.body = this.GetComponent<Rigidbody>();
+        this.gun = this.transform.GetChild(1);
     }
 
     private void FixedUpdate()
@@ -45,11 +47,25 @@ public class PlayerController : MonoBehaviour
                 rotation = -rotationSpeed;
             else
                 rotation = 0;
+
+            if (Input.GetKey(KeyCode.Space))
+                this.Shoot();
         }
 
         //apply friction
         body.MovePosition(move);
         rotationAngle = Quaternion.Euler(0, rotationAngle.eulerAngles.y + rotation, 0);
         transform.rotation = rotationAngle;
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        // Shoots a ray from the gun, if the collider it hits is on layer 6, the "enemies" layer, it will be destroyed
+        if (Physics.Raycast(this.gun.transform.position, this.gun.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, 1 << 6))
+        {
+            Debug.DrawRay(this.gun.transform.position, this.gun.transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Destroy(hit.collider.gameObject);
+        }
     }
 }
